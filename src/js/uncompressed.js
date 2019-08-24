6112de47385251fa8613abd3f473db344c4c9e2e
odinokun <odinokun@gmail.com>
2019-08-24 15:46:59 +0300

    did displacement map for text

--- src/js/uncompressed.js
+++ src/js/uncompressed.js
@@ -20,21 +20,26 @@ document.body.appendChild(app.view);
 
 
 const container = new PIXI.Container();
+const TextContainer = new PIXI.Container();
+const BgContainer = new PIXI.Container();
 app.stage.addChild(container);
+app.stage.addChild(BgContainer);
+app.stage.addChild(TextContainer);
 
 
-// Create a new texture
+//begin new texture
 const bg = new PIXI.Sprite.from('assets/img/bg.jpg');
 bg.width = app.screen.width;
 bg.height = app.screen.height;
 
-container.addChild(bg);
+BgContainer.addChild(bg);
+//end new texture
 
 
-// Create a new text
+// begin new text
 const basicText = new PIXI.Text('Odinokun`s test pixi.js', {
   fontFamily: 'Arial Narrow',
-  fontSize: 120,
+  fontSize: 100,
   fontWeight: 'bold',
   fill: '#ffffff',
   wordWrap: true,
@@ -43,11 +48,35 @@ const basicText = new PIXI.Text('Odinokun`s test pixi.js', {
 basicText.x = 30;
 basicText.y = 90;
 
-container.addChild(basicText);
+TextContainer.addChild(basicText);
+//end new text
+
+
+//begin displacement map
+const displacementSprite = PIXI.Sprite.from('assets/img/displacement.png');
+// const displacementSprite = PIXI.Sprite.from('assets/img/noise.png');
+const displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+
+app.stage.addChild(displacementSprite);
+
+TextContainer.filters = [displacementFilter]; //применяем фильтр к контейнеру с текстом
+displacementFilter.scale.x = 0.1; //сила искажения фильтра
+displacementFilter.scale.y = 0.1; //сила искажения фильтра
+//displacementSprite.anchor.set(0.5); //это мы позиционируем центр картинки
+//end displacement map
 
 
 // Listen for animate update
 app.ticker.add((delta) => {
   // рендерим контейнер с фоном
   app.renderer.render(container);
+});
+
+document.body.addEventListener('click', () => {
+  let tl = new TimelineMax();
+  tl
+    //.to(displacementFilter.scale, 1, {x: 70, y: 70}) //анимируем силу искажения фильтра
+    .to(displacementFilter.scale, 1, {x: 600, y: 200}) //анимируем силу искажения фильтра
+    .to(basicText.position, 1, {x: 100},0) //анимируем позицию текста (0 в конце, для параллельности анимации)
+    .to(basicText, 1, {alpha: 0},0); //анимируем прозрачность текста (0 в конце, для параллельности анимации)
 });
